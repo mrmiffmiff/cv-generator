a73c625ec2b7ef6ceb127e7a21d5a9f8973f4d1a
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-11 12:37:50 -0800

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import './App.css'
 function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <div className='app-shell'>
+      <section className='form-side'>
+        <header>
+          <h1>CV Builder</h1>
+          <p>Fill in personal details on the left. The preview updates live.</p>
+        </header>
+        {/*Okay so after this we're going to need a few... FormSections, essentially, perhaps, to represent the areas for Personal Details, for Education, and for Experience*/}
+      </section>
+      {/*Then after all the forms we need to have on the right side the actual resume preview.*/}
+      <section className='preview-side'>
+        <div className='preview-page'>
+
+        </div>
+      </section>
+    </div>
   )
 }
 
